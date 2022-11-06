9ad761bf54c73cafefac156a7a4edc5a1ce6b951
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-11-06 07:27:00 +0000

    [Add] devise導入、各controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+
+  # 管理者
+  devise_for :admins, controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :admin do
+    get "top" => "homes#top", as: "top"
+  end
+
+  # 顧客
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  scope module: :public do
+    root to: "homes#top"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

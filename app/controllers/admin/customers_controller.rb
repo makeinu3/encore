ff4933fc7e562715613539110cc0d8c98b35be8d
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-11-10 04:21:16 +0000

    [Add] admin/customer/show,edit,update追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -15,9 +15,28 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+    # サイドバー用
+    @customers = Customer.all
+    @posts = Post.all.order("created_at DESC")
+    @messages = Message.all
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+    # サイドバー用
+    @customers = Customer.all
+    @posts = Post.all.order("created_at DESC")
+    @messages = Message.all
+  end
+
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:published, :is_deleted)
   end
 
 end

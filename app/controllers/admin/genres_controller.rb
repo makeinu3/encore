4a4114b81996864abf115c1b3e5ed2ede8d165dd
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-11-12 03:02:04 +0000

    [Fix] genre/create,update機能修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -3,22 +3,38 @@ class Admin::GenresController < ApplicationController
   def index
     @genre = Genre.new
     @genres = Genre.all
+    # サイドバー用
+    @customers = Customer.all
+    @posts = Post.all.order("created_at DESC")
+    @messages = Message.all
   end
 
   def create
     @genre = Genre.new(genre_params)
     @genre.save
     redirect_to admin_genres_path
+    # サイドバー用
+    @customers = Customer.all
+    @posts = Post.all.order("created_at DESC")
+    @messages = Message.all
   end
 
   def edit
     @genre = Genre.find(params[:id])
+    # サイドバー用
+    @customers = Customer.all
+    @posts = Post.all.order("created_at DESC")
+    @messages = Message.all
   end
 
   def update
     @genre = Genre.find(params[:id])
     @genre.update(genre_params)
     redirect_to admin_genres_path
+    # サイドバー用
+    @customers = Customer.all
+    @posts = Post.all.order("created_at DESC")
+    @messages = Message.all
   end
 
 

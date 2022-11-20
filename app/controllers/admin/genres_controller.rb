e50534d6ee8d1673f63c7d47fb822a1fbb8d5d51
makeinu3 <112616049+makeinu3@users.noreply.github.com>
2022-11-20 12:05:29 +0000

    [Fix] fontawesome5.15.4のコードに修正

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,7 +2,7 @@ class Admin::GenresController < ApplicationController
 
   before_action :authenticate_admin!
   before_action :set_sidebar                  # @customers, @posts, @messages
-  before_action :set_genre, except: [:index]  # Genre.find(params[:id])
+  before_action :set_genre, except: [:index, :create]  # Genre.find(params[:id])
 
   def index
     @genre = Genre.new
